289c52f3a4e5eb4751f36884d09b6f413fcb6624
Bruno Santos <brunosansp@outlook.com>
2023-05-16 15:32:20 -0300

    Front-end add select, snackbar and save course form

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,36 +1,51 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: "app-course-form",
-  templateUrl: "./course-form.component.html",
-  styleUrls: ["./course-form.component.scss"],
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar
+  ) {
     this.form = formBuilder.group({
-      _id: [""],
+      _id: [''],
       name: [
-        "",
+        '',
         [
           Validators.required,
           Validators.minLength(5),
           Validators.maxLength(100),
         ],
       ],
-      category: ["", [Validators.required]],
+      category: ['', [Validators.required]],
     });
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    throw new Error("Method not implemented.");
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result),
+      (error) => this.onError()
+    );
   }
 
   onCancel() {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
+  }
+
+  onError() {
+    this._snackBar.open('Error ao salvar curso.', undefined, {
+      duration: 5000,
+    });
   }
 }
